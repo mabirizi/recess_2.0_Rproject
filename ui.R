811da24d6eccceb0ddf7d392ab7caec51f937b45
nassersaazi <nassersnoop@gmail.com>
2018-07-26 00:37:22 +0300

    Debugged race and age analysis code

---
+++ ui.R
@@ -0,0 +1,9 @@
+
+library(shinydashboard)
+shinyUI(fluidPage(
+  tags$style("body{background-color:whitesmoke}", "h1{text-align:center;color:steelblue}"),
+  
+  h1("STACK OVERFLOW ANALYSIS SYSTEM"),uiOutput("stack")
+  
+))
+
